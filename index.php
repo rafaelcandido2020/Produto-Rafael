346c0a5963620a596da6f4363a1fb77a588a8a95
RAFAEL GONCALVES CANDIDO <987307054@itaud.des.ihf>
2021-03-03 14:21:20 -0300

    versionamento do meu primeiro site em produção

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Produto Rafael</title>
+</head>
+<body>
+
+<h1>Produto Rafael</h1>
+<p>Profissional de Gestão de Projetos com 7 anos de experiencia.</p>
+
+</body>
+</html>
