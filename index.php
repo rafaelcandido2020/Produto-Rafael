f01594b1e2c3d9f7ee1ea17dfe521cb107e6bd6d
Rafael Candido <rafael.candidog@gmail.com>
2021-03-03 16:35:15 -0300

    alteração 3

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
       <p>Profissional de Gestão de Projetos com 7 anos de experiencia.</p>
   
   <div style= "width: 100%; display:flex;height: 80px">
-  <div style= "width: 50%;background:blue">
-  <div style= "width: 50%;background:red">
+  <div style= "width: 50%;background:blue"></div>
+  <div style= "width: 50%;background:red"></div>
 </div>
 
   </body>
